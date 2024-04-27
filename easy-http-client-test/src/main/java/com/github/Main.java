0c77ebd8e8c9d118b4cff4465022f6b914172cd0
wangxj <xiaojiangzt@gmail.com>
2024-04-28 00:15:03 +0800

    feat: 新增Get请求测试类(未完成)

--- easy-http-client-test/src/main/java/com/github/Main.java
+++ easy-http-client-test/src/main/java/com/github/Main.java
@@ -12,4 +12,5 @@ public class Main {
         com.github.easyhttp.core.Main main = new com.github.easyhttp.core.Main();
         main.printf();
     }
+
 }
---
+++ easy-http-client-test/src/main/java/com/github/easyhttp/test/GetRequestTest.java
@@ -0,0 +1,11 @@
+package com.github.easyhttp.test;
+
+/**
+ * Description
+ *
+ * @author wangxiaojiang
+ * @version v1.0.0
+ * @create 2024/4/28 0:13
+ */
+public class GetRequestTest {
+}
