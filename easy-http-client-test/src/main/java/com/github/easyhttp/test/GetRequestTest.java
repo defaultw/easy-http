2607e33aa128d42a8c3ca23e7325cdd0c61dbd58
wangxj <xiaojiangzt@gmail.com>
2024-05-06 23:05:42 +0800

    feat: 增加使用示例

--- easy-http-client-test/src/main/java/com/github/easyhttp/test/GetRequestTest.java
+++ easy-http-client-test/src/main/java/com/github/easyhttp/test/GetRequestTest.java
@@ -1,5 +1,7 @@
 package com.github.easyhttp.test;
 
+import com.github.easyhttp.core.builder.HttpGetRequestBuilder;
+
 /**
  * Description
  *
@@ -8,4 +10,16 @@ package com.github.easyhttp.test;
  * @create 2024/4/28 0:13
  */
 public class GetRequestTest {
+
+
+    public static void main(String[] args) {
+
+        String s = new HttpGetRequestBuilder().uri("http://localhost:8080/easy-http/getTest").executeAsString();
+
+        System.out.println(s);
+
+
+    }
+
+
 }
---
+++ easy-http-client-test/src/main/java/com/github/easyhttp/test/PostRequestTest.java
@@ -0,0 +1,11 @@
+package com.github.easyhttp.test;
+
+/**
+ * Description
+ *
+ * @author wangxiaojiang
+ * @version v1.0.0
+ * @create 2024/5/6 23:03
+ */
+public class PostRequestTest {
+}
